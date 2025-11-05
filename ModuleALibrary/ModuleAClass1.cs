088539790a9f12466a54ab83907e816df23a56b1
Anton Pavlov <anton.pavlov@farmis.cz>
2025-11-05 16:28:04 +0100

    Some projects.

---
+++ ModuleALibrary/ModuleAClass1.cs
@@ -0,0 +1,15 @@
+﻿namespace ModuleALibrary;
+
+public interface IModuleAInterface
+{
+    string GetData();
+}
+
+public class ModuleAClass1: IModuleAInterface
+{
+    public string GetData()
+    {
+        return "Data from Module A Class 1";
+    }
+
+}
---
+++ ModuleATests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ModuleATests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
